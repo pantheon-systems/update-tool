f72d6e8bc0129f584e3d6980a98ef900c2c28df1
Greg Anderson <greg.1.anderson@greenknowe.org>
2018-09-11 14:04:46 -0700

    Also enable PR logging

--- src/Cli/ApiTrait.php
+++ src/Cli/ApiTrait.php
@@ -3,6 +3,7 @@
 namespace Updatinate\Cli;
 
 use Hubph\HubphAPI;
+use Symfony\Component\Filesystem\Filesystem;
 
 trait ApiTrait
 {
@@ -15,6 +16,14 @@ trait ApiTrait
     {
         $api = new HubphAPI($this->getConfig());
         $api->setAs($as);
+
+        // Turn on PR request logging if the log path is set.
+        $log_path = $this->getConfig()->get('log.path');
+        if ($log_path) {
+            $fs = new Filesystem();
+            $fs->mkdir(dirname($log_path));
+            $api->startLogging($log_path);
+        }
 
         return $api;
     }
