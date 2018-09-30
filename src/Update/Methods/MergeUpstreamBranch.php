96aeb123241a15ca69b9bf3b34fba5569201ebe9
Greg Anderson <greg.1.anderson@greenknowe.org>
2018-09-30 01:09:39 -0700

    Fix up 'MergeUpstreamBranch' placeholder class.

--- src/Update/Methods/MergeUpstreamBranch.php
+++ src/Update/Methods/MergeUpstreamBranch.php
@@ -2,6 +2,8 @@
 
 namespace Updatinate\Update\Methods;
 
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Symfony\Component\Filesystem\Filesystem;
 use Updatinate\Git\WorkingCopy;
 
@@ -10,9 +12,12 @@ use Updatinate\Git\WorkingCopy;
  * the upstream repository and merges them into the working copy of the
  * project being updated.
  */
-class MergeUpstreamBranch extends UpdateMethodBase
+class MergeUpstreamBranch implements UpdateMethodInterface, LoggerAwareInterface
 {
-    public function update(WorkingCopy $project, WorkingCopy $upstream)
+    use UpdateMethodTrait;
+    use LoggerAwareTrait;
+
+    public function update(WorkingCopy $project, WorkingCopy $upstream, array $parameters)
     {
 
     }
