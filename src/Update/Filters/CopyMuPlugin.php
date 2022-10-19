da6ebc39dc2ba94fa05fecc640b026e707550b55
Chris Reynolds <chris@jazzsequence.com>
2022-10-19 10:36:47 -0600

    more cs fixes

--- src/Update/Filters/CopyMuPlugin.php
+++ src/Update/Filters/CopyMuPlugin.php
@@ -64,7 +64,7 @@ class CopyMuPlugin implements UpdateFilterInterface, LoggerAwareInterface
         }
 
         // Check if the /pantheon subdirecotry exists. If it does, delete that, too.
-        if ($fs->exists( $muplugin_dir . '/pantheon' )) {
+        if ($fs->exists($muplugin_dir . '/pantheon')) {
             $this->logger->notice('Removing {file}', ['file' => $dest . '/pantheon']);
             $fs->remove($dest . '/pantheon');
         }
