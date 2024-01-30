2ff2630d0bb15b822ee44cb71307ec498bceef14
Chris Reynolds <chris@jazzsequence.com>
2024-01-30 11:44:32 -0700

    echo something at the beginning of update_json

--- tests/bin/prepare-wp-upstreams.sh
+++ tests/bin/prepare-wp-upstreams.sh
@@ -56,6 +56,7 @@ for project in "${projects[@]}"; do
 done
 
 function update_json() {
+	echo "Checking diff between '${working_copy_path}/wp' and '${working_copy_path}/wpms'..."
 	diff=$(diff "${working_copy_path}/wp" "${working_copy_path}/wpms")
 	if [ $diff > /dev/null ]; then
 		echo "wp fixture is already ahead of wpms fixture. Skipping update."
