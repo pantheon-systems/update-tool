c21a377978ae339dd617d834ed9b28fc3710f9c3
Kevin Porras <kevin.porras@pantheon.io>
2025-05-21 07:34:25 -0600

    Only replace terminus 4 download link.

--- src/Cli/UpdateTerminusDocsCommands.php
+++ src/Cli/UpdateTerminusDocsCommands.php
@@ -146,7 +146,7 @@ class UpdateTerminusDocsCommands extends \Robo\Tasks implements ConfigAwareInter
 
         $installMdContents = file_get_contents($dir . '/source/content/terminus/02-install.md');
         $replacement = '${1}' . $terminusRelease . '${3}';
-        $installMdContents = preg_replace('/(.*https:\/\/github\.com\/pantheon-systems\/terminus\/releases\/download\/)(\d+\.\d+\.\d+)(.*)/', $replacement, $installMdContents);
+        $installMdContents = preg_replace('/(.*https:\/\/github\.com\/pantheon-systems\/terminus\/releases\/download\/)(4\.\d+\.\d+)(.*)/', $replacement, $installMdContents);
         file_put_contents($dir . '/source/content/terminus/02-install.md', $installMdContents);
 
 
